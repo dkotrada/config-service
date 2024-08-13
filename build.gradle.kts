6b214fbd077188e9771d09dee9a4d538e65b08dc
dkotrada <dymmy@mail.com>
2024-08-13 17:14:26 +0200

    Init Config Service

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.polarbookshop"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.3"
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+dependencies {
+	implementation("org.springframework.cloud:spring-cloud-config-server")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
